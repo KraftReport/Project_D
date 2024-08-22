03bca8801836b4053239d0c4ed3f948aebba8a5a
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2024-08-22 21:58:33 +0630

    file upload

--- event/src/main/java/com/kraft/event/service/helper/FileUploadService.java
+++ event/src/main/java/com/kraft/event/service/helper/FileUploadService.java
@@ -23,8 +23,7 @@ public class FileUploadService {
             var location = Paths.get("src/main/resources/static/files").toAbsolutePath().normalize();
             if(!Files.exists(location)){
                 Files.createDirectories(location);
-            }
-            assert fileName != null;
+            } 
             var filePath = location.resolve(fileName);
             Files.copy(multipartFile.getInputStream(),filePath,REPLACE_EXISTING);
             return filePath.toString();
