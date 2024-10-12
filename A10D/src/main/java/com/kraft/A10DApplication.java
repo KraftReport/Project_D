a7fe3f7fbf2155b596403fbe43a56a34a6bc1150
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2024-10-12 22:45:46 +0630

    a 10 d

---
+++ A10D/src/main/java/com/kraft/A10DApplication.java
@@ -0,0 +1,13 @@
+package com.kraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A10DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A10DApplication.class, args);
+	}
+
+}
